b836b1ed722164ee7ea2a2aafff4f115f0f52387
GuilhermeHalter <guisam.halter@gmail.com>
2023-09-04 09:23:21 -0300

    corrigidos os erros

--- RedFox/models/__init__.py
+++ RedFox/models/__init__.py
@@ -1,5 +1,5 @@
 from .categoria import Categoria
+from .time import Time
+from .jogo import Jogos
 from .estadios import Estadios
 from .ingresso import Ingresso
-from .jogo import Jogos
-from .time import Time
